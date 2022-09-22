46e03f286818d2cecd9f28d2fdfd2705199aee13
sunpatchlbr <89615852+sunpatchlbr@users.noreply.github.com>
2022-09-22 10:46:42 -0500

    first draft

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string>
+
+bool containsKeyword(Document docToSearch, string keyword)
+{
+    if(docToSearch->GetContents().find(keyword) != std::string::npos)
+        return true;
+    return false;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
